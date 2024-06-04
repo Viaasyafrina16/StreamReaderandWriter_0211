c3f8d94ba1f520b9084c3198cf78b75b41bc2c44
Viaasyafrina16 <syafrinavia@gmail.com>
2024-06-04 15:19:17 +0700

    membuat catch

--- manualException.cpp
+++ manualException.cpp
@@ -4,6 +4,11 @@ using namespace std;
 int main()
 {
     try {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw 0.5; //melemparkan sebuah  integer maka
+        cout << "Pengecualian akan dieksekusi" << endl;
+    }
+    catch {
         
     }
 }
